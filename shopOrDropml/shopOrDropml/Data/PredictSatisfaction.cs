2f1101036082f3a45319ebba54985dcb21d4292f
franceneee <57038559+franceneee@users.noreply.github.com>
2022-12-04 13:15:28 +0800

    Attempt to train

--- shopOrDropml/shopOrDropml/Data/PredictSatisfaction.cs
+++ shopOrDropml/shopOrDropml/Data/PredictSatisfaction.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML.Data;
 
 namespace shopOrDropml.Data
 {
-    internal class PredictSatisfaction
+    public class PurchaseData
     {
+        [LoadColumn(0)]
+        public string? DayofWeek;
+
+        [LoadColumn(1)]
+        public string? Category;
+
+        [LoadColumn(2)]
+        public float ItemCost;
+
+        [LoadColumn(3)]
+        public int Satisfaction;
+
+        [LoadColumn(4)]
+        public bool Online;
+    }
+
+    public class SatisfactionPrediction
+    {
+        [ColumnName("Score")]
+        public float Satisfaction;
     }
 }
