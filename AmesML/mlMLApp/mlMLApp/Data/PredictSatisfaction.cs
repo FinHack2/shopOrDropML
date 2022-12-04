e176a0082e086d30241ddb5a46f9961fafb9fc45
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2022-12-04 14:00:18 +0800

    Model with API

---
+++ AmesML/mlMLApp/mlMLApp/Data/PredictSatisfaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace shopOrDropml.Data
+{
+    public class PurchaseData
+    {
+        [LoadColumn(0)]
+        public string? DayofWeek;
+
+        [LoadColumn(1)]
+        public string? Category;
+
+        [LoadColumn(2)]
+        public float ItemCost;
+
+        [LoadColumn(3)]
+        public int Satisfaction;
+
+        [LoadColumn(4)]
+        public bool Online;
+    }
+
+    public class SatisfactionPrediction
+    {
+        [ColumnName("Score")]
+        public float Satisfaction;
+    }
+}
